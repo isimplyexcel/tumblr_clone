4846dfdc7ccccd51b7916fe1cdde91e30b34eadf
isimplyexcel <isimplyexcel@gmail.com>
2019-10-12 22:39:55 -0700

    tumblr clone

---
+++ Tumblr_2019/Pods/Target Support Files/Pods-Tumblr_2019-Tumblr_2019UITests/Pods-Tumblr_2019-Tumblr_2019UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tumblr_2019_Tumblr_2019UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Tumblr_2019_Tumblr_2019UITests
+@end
---
+++ Tumblr_2019/Tumblr_2019/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  Tumblr_2019
+//
+//  Created by Paul Min on 10/12/19.
+//  Copyright © 2019 Paul Min. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
